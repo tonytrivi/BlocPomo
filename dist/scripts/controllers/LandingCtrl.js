494e2ad3e84b569c0d570feb1c35f085dfd78557
Tony Trivison <shambles72@hotmail.com>
2016-08-31 18:10:47 -0700

    wrapped startSession

--- dist/scripts/controllers/LandingCtrl.js
+++ dist/scripts/controllers/LandingCtrl.js
@@ -1,11 +1,34 @@
 (function() {
      function LandingCtrl($firebaseObject,TimerManager) {
          this.timerManager = TimerManager;
+         this.RESET = "Reset";
+         this.START_SESSION = "Start Session";
+         this.sessionDisplay = this.START_SESSION;
+         
+         this.startSession = function () {
+             if (!TimerManager.running) {
+                this.sessionDisplay = this.RESET;
+                this.timerManager.startSession();
+             }
+             else {
+                this.sessionDisplay = this.START_SESSION;
+                this.timerManager.startSession();
+                 
+             }
+             
+         
+         };
+         
+
+         
+         
     
          
          
-         var dbRef = firebase.database().ref().child('tony');
-         this.object = $firebaseObject(dbRef);
+         //firebase experimenting
+         //var dbRef = firebase.database().ref().child('tony');
+         //this.object = $firebaseObject(dbRef);
+         
          //this.sayHello = () => { return `${this.object.name}`; }
          //this.sayHello = this.object.name;
          
