5a529f14ae9e6b2d61fc8eed73b62f103d8ae3a4
Tony Trivison <shambles72@hotmail.com>
2016-09-17 15:09:53 -0700

    went back to script path for timeconverter

--- dist/scripts/filters/timeconverter.js
+++ dist/scripts/filters/timeconverter.js
@@ -1,7 +1,7 @@
 (function() {
      function timeConverter() {         
          return function(seconds) {
-
+             //convert the date
              var output = new Date(1970, 0, 1).setSeconds(seconds);
 
              return output;
